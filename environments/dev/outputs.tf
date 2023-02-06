2770b7f9076313445400a5d0de5b8a4b5d9c565b
wrhewitt <randonhewitt@gmail.com>
2023-02-05 21:20:08 -0600

    Refromat, adding in cloud run

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,18 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 output "network" {
   value = "${module.vpc.network}"
 }
@@ -25,10 +10,12 @@ output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
 
-output "instance_name" {
-  value = "${module.http_server.instance_name}"
+output "service_url" {
+  value       = "${module.google_cloud_run_service.main.status[0].url}"
+  description = "The URL on which the deployed service is available"
 }
 
-output "external_ip" {
-  value = "${module.http_server.external_ip}"
+output "verified_domain_name" {
+  value       = values("${module.google_cloud_run_domain_mapping.domain_map}")[*].name
+  description = "List of Custom Domain Name"
 }
