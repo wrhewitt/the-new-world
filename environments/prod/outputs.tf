0c9772a20696a202645b35a8b8dde4088cb110b8
wrhewitt <randonhewitt@gmail.com>
2023-02-05 21:23:52 -0600

    wrong outputs

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -10,13 +10,3 @@ output "subnet" {
 output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
-
-output "service_url" {
-  value       = "${module.google_cloud_run_service.main.status[0].url}"
-  description = "The URL on which the deployed service is available"
-}
-
-output "verified_domain_name" {
-  value       = values("${module.google_cloud_run_domain_mapping.domain_map}")[*].name
-  description = "List of Custom Domain Name"
-}
